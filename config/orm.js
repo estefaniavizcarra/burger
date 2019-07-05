81a72913abe6175bede07b4e6d716b340d9d8095
Estefanía Vizcarra <estefaniavizcarra@MacBook-Air-de-Estefania-2.local>
2019-07-05 17:52:27 -0500

    ya hice base de datos con burger pero la cambiare a candies

--- config/orm.js
+++ config/orm.js
@@ -13,3 +13,102 @@ function printQuestionMarks(num) {
   
     return arr.toString();
   }
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+      var value = ob[key];
+      // check to skip hidden properties
+      if (Object.hasOwnProperty.call(ob, key)) {
+        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+        // e.g. {sleepy: true} => ["sleepy=true"]
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+  }
+  // READ/SELECT all the records in the database
+// ===========================================================
+var orm = {
+    all: function(tableInput, br) {
+      var queryString = 'SELECT * FROM ' + tableInput + ';';
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+        br(result);
+      });
+    },
+  
+    // CREATE/INSERT a new record to the database
+    // ===========================================================
+    create: function(table, cols, vals, br) {
+      var queryString = 'INSERT INTO ' + table;
+  
+      queryString += ' (';
+      queryString += cols.toString();
+      queryString += ') ';
+      queryString += 'VALUES (';
+      queryString += printQuestionMarks(vals.length);
+      queryString += ') ';
+  
+      console.log(queryString);
+  
+      connection.query(queryString, vals, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        br(result);
+      });
+    },
+  
+    // UPDATE/UPDATE a specific record in the database
+    // ===========================================================
+    update: function(table, objColVals, condition, br) {
+      var queryString = 'UPDATE ' + table;
+  
+      queryString += ' SET ';
+      queryString += objToSql(objColVals);
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      console.log(queryString);
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        br(result);
+      });
+    },
+  
+    // DELETE/DELETE a specific record in the database
+    // ===========================================================
+    delete: function(table, condition, br) {
+      var queryString = 'DELETE FROM ' + table;
+      queryString += ' WHERE ';
+      queryString += condition;
+  
+      connection.query(queryString, function(err, result) {
+        if (err) {
+          throw err;
+        }
+  
+        br(result);
+      });
+    },
+  };
+  
+  // Export the orm object for the model (../models/burger.js).
+  module.exports = orm;
+  
